dd9a40d7c814a8c441ef4abc1a732338c97d5b62
RAFIE Younes <younes.rafie@gmail.com>
2015-03-14 13:55:33 +0000

    Added the doc configuration in my repo and linked directly to the Laravel official repo && changes doc.yml localization

--- config/config.php
+++ config/config.php
@@ -3,14 +3,14 @@
 use RAFIE\Configuration;
 use Symfony\Component\Finder\Finder;
 
-$dir = __DIR__ . '/../LaravelDocs';
+$dir = __DIR__ . '/../docs';
 
 $finder = Finder::create()->files()->in($dir);
 
 $versions = RAFIE\Version\GitVersionCollection::create($dir)
     ->add('master', 'Master')
-    ->add('5.0', '5.0');
-// I didn't use multiple versions, like 4.2 because I have to create a doc.yml to describe the documentation structure.
+    ->add('5.0', '5.0')
+    ->add('4.2', '4.2');
 
 $options = [
     'theme'       => 'laravel',
@@ -20,6 +20,7 @@ $options = [
     //'parser'      => '\\RAFIE\\Parser\\MarkdownParser'
 ];
 
-$docConf = $dir . '/doc.yml';
+// the documentation file can also be under version control, I'm using it this way because i'm using the Laravel doc repo as a submodule
+$docConf = $dir . '/../doc.yml';
 
 return new Configuration($finder, $docConf, $options);
